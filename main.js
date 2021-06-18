5eeb95665fb75dcfe5f14f2ab6b8bbc4340172bd
Sarvadnya mahajan <80883695+SarvadnyaMahajan@users.noreply.github.com>
2021-06-18 15:48:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,29 @@ function start() {
 
 recognition.onresult=function(event) {
     console.log(event)
-    var Content = event.result[0][0].transcript
+    var Content = event.results[0][0].transcript
     document.getElementById("textbox").innerHTML=Content
     console.log(Content)
+    speak()
 } 
+
+ function speak() {
+     var syntheis=window.speechSynthesis
+
+     speak_data=document.getElementById("textbox").value
+
+     var utterThis= new SpeechSynthesisUtterance(speak_data)
+
+     syntheis.speak(utterThis)
+
+     Webcam.attach(camera);
+ }
+ camera=document.getElementById("camera")
+ Webcam.set({
+    width: 360,
+    height: 260,
+    image_format: 'png',
+    png_quality: 90
+  });
+  
+  

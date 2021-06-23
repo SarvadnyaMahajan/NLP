50651b312ad548a2f0848ddbeffa74afc5e66858
Sarvadnya mahajan <80883695+SarvadnyaMahajan@users.noreply.github.com>
2021-06-23 15:55:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,20 +11,29 @@ recognition.onresult=function(event) {
     var Content = event.results[0][0].transcript
     document.getElementById("textbox").innerHTML=Content
     console.log(Content)
+    if (Content=="take my selfie") {
+    console.log("taking selfie")
     speak()
+    }
 } 
 
  function speak() {
      var syntheis=window.speechSynthesis
 
-     speak_data=document.getElementById("textbox").value
+     speak_data="Taking your Selfie in 5 second"
 
      var utterThis= new SpeechSynthesisUtterance(speak_data)
 
      syntheis.speak(utterThis)
 
      Webcam.attach(camera);
+
+     setTimeout(function(){
+        takesnapshot()
+        save()
+     },5000)
  }
+
  camera=document.getElementById("camera")
  Webcam.set({
     width: 360,
@@ -33,4 +42,15 @@ recognition.onresult=function(event) {
     png_quality: 90
   });
   
-  
+  function takesnapshot() {
+      Webcam.snap(function(data_uri){
+          document.getElementById("reward").innerHTML = '<img id = "selfie_image"  src = "'+data_uri+'">'
+      }) 
+  }
+
+  function save() {
+      link=document.getElementById("link")
+      img=document.getElementById("selfie_image").src 
+      link.href=img
+      link.click() 
+  }  
